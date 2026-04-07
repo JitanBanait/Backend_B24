7c064aae7019531981c009d5d8f8926f4eac9826
abc <abc@.com>
2026-04-07 10:52:42 +0530

    Todo List onrefresh

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-let express = require("express");
-let app = express();
-let fs = require("fs")
+const express = require("express")
+const fs = require("fs")
+const app = express();
 const port = 8000;
+app.use(express.static("public"));
+app.use(express.json());
 
-app.use(express.static('public'))
-app.use("/contact",(req, res , next)=>{
-            console.log(req.url)
-            req.abc = "hello"
-            console.log("Middleware");
-            next();
+
+app.get("/getTodo",(req,res)=>{
+  
+   fs.readFile("./store.txt",(err , data)=>{
+    if(err) {res.sendStatus(400)}
+    else{
+        res.send(data);
+    }
+   })
+       
 })
 
-app.use("/contact",(req, res , next)=>{
-            console.log(req.url)
-            req.abc = "hello"
-            console.log("Middleware2");
-            next();
-})
-
-app.get("/products/:var",(req,res)=>{
-            console.log(req.params.var);
-            let item = req.params.var;
-           res.send(req.abc)
-    }) 
+app.post("/addTodo" , (req, res)=>{
+    let tasks = [];
+    let data = fs.readFileSync("./store.txt");
+        if(data == ""){
+            tasks = [];
+         }else{
+            tasks = JSON.parse(data);
+         }
+   
     
 
-
-app.get("/contact",(req,res)=>{
-    console.log(req.url)
-    res.send("Contact Page");
+    tasks.push(req.body)
+    
+    fs.writeFile("./store.txt",JSON.stringify(tasks),(err)=>{
+        if(err){
+            res.send("err");
+        }else{
+            res.send("True");
+        }
+    })
 })
 
 app.listen(port , ()=>{
-    console.log("Server is runing");
+    console.log("Server is running on " , port) ;
 })
--- public/script.js
+++ public/script.js
@@ -1,14 +1,49 @@
-let h1 = document.getElementById("h1");
-let btn = document.getElementById("test");
+let input = document.getElementById("input");
+let btn = document.getElementById("btn");
+let list = document.getElementById("list")
+
+function getTodos(){
+     let xml = new XMLHttpRequest();
+    
+    xml.open("get" , "http://localhost:8000/getTodo");
+    xml.send();
+    xml.onload = (resp)=>{
+        console.log(resp);
+        if(resp.target.status == 400){
+            console.log("err")
+        }else{
+            let data= JSON.parse(resp.target.responseText)
+            data.forEach(element => {
+                createLi(element)
+            });
+        }
+    }
+}
+getTodos();
+
+function createLi(task){
+    let li = document.createElement("li");
+    li.innerText = task.task;
+    list.appendChild(li);
+}
+
+
 
 btn.addEventListener("click",(e)=>{
+
     let xml = new XMLHttpRequest();
     let task = {
-        task : "test1",
+        task : input.value,
         date : Date.now()
     }
     xml.open("post" , "http://localhost:8000/addTodo");
     xml.setRequestHeader('Content-type' , "application/json")
-
     xml.send(JSON.stringify(task));
+    xml.onload = (resp)=>{
+        console.log(resp);
+        if(resp.target.responseText == "True"){
+            createLi(task);
+        }
+    }
+    
 })
